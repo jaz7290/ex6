827619edb1fcad885ce77f462e5ff38a39c39ab8
Jaspreet Singh <ext-jaspreet.4.singh@here.com>
2013-11-26 12:02:18 +0100

    added new data folder to sidebar demo

---
+++ maps_api_for_javascript/demos/sidebar-component/data/election-result-data-for-german-states.js
@@ -0,0 +1,18 @@
+﻿ 
+var popData = [{"latitude":53.633333,"longitude":11.416667,"values":[27,18,7,0,14,5],"name":"Mecklenburg-Vorpommern"},
+{"latitude":52.133333,"longitude":11.616667,"values":[26,42,9,0,28,0],"name":"Saxony-Anhalt"},
+{"latitude":49.23149,"longitude":6.99833,"values":[18,19,2,0,8,0],"name":"Saarland"},
+{"latitude":50.0006,"longitude":8.2723,"values":[42,41,	18,0,0,0],"name":"Rhineland-Palatinate"},
+{"latitude":54.32324,"longitude":10.1322,"values":[22,22,10,6,0,9],"name":"Schleswig-Holstein"},
+{"latitude":50.08459,"longitude":8.2423,"values":[29,46,17,20,6,0],"name":"Hesse"},
+{"latitude":53.07509,"longitude":8.8047,"values":[36,20,21,0,5,1],"name":"Bremen"},
+{"latitude":51.21563,"longitude":6.77604,"values":[99,67,29,22,0,0],"name":"North Rhine-Westphalia"},
+{"latitude":51.05363,"longitude":13.74081,"values":[14,58,9,14,29,8],"name":"Saxony"},
+{"latitude":52.39935,"longitude":13.04793,"values":[30,19,5,7,25,2],"name":"Brandenburg"},
+{"latitude":48.13641,"longitude":11.57753,"values":[39,92,19,16,0,21],"name":"Bavaria"},
+{"latitude":48.76767,"longitude":9.17192,"values":[35,60,36,7,0,0],"name":"Baden-Wuerttemberg"},
+{"latitude":52.37227,"longitude":9.73815,"values":[47,69,12,13,10,1],"name":"Lower Saxony"},
+{"latitude":52.51607,"longitude":13.37698,"values":[47,38,29,0,19,16],"name":"Berlin"},
+{"latitude":53.55334,"longitude":9.99245,"values":[62,28,14,9,8,0],"name":"Hamburg"},
+{"latitude":50.983333,"longitude":11.033333,"values":[19,30,6,7,26,0],"name":"Thuringia"}]
+
